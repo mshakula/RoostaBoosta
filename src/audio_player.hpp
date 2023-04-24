9aaa12c19b2c2fafc2c62be80d3837e8d0be2244
jcurry39 <jake.curry@ymail.com>
2023-04-24 16:24:12 -0400

    Added audio_player files

---
+++ src/audio_player.hpp
@@ -0,0 +1,21 @@
+/// \file audio_player.hpp
+/// \date 2023-04-22
+/// \author jcurry39 (jake.curry@ymail.com)
+///
+/// \brief header fuctions to control the speaker
+
+#ifndef AUDIO_PLAYER_H
+#define AUDIO_PLAYER_H
+#include "weather_data.hpp"
+
+// ======================= Public Interface ==========================
+
+/// \brief reads the weather data on the speaker
+///
+/// \param data The weather data to print.
+void 
+play_audio(weather_data* data);
+
+// ===================== Detail Implementation =======================
+
+#endif // AUDIO_PLAYER_H
