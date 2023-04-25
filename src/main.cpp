07c25b0eed3f2e91e2f62579a8ef8239ff654929
Raymond Barrett <rbarrett38@gatech.edu>
2023-04-25 18:02:28 -0400

    wifi working interface with struct

--- src/main.cpp
+++ src/main.cpp
@@ -42,33 +42,92 @@ printdir()
 
 } // namespace
 
-// ====================== Global Definitions =========================
-WifiClient wifi(p28, p27, p26);
+namespace{
+
+WifiClient wifi(p28, p27, p26);    
 char ssid[32] = "test";     // enter WiFi router ssid inside the quotes
 char pwd [32] = "test1234"; // enter WiFi router password inside the quotes
+const char* addr = "api.weatherapi.com";
+const char* payload = "/v1/forecast.json?key=a9e3fb6a760c49699d625304232504&q=Atlanta&aqi=no";
+const char* header = "Accept: application/xml";
 
+int 
+startWifi()
+{
+    wifi.init();
+    //char ap_list[256];
+    //wifi.scan(ap_list, 256);
+    wifi.connect(ssid,pwd);
+    return 1;
+}
+
+int
+searchChars(char* chars, const char* query, size_t chars_size, size_t query_size, int start_pos=0)
+{
+    bool flag;
+    for(int i=start_pos; i<chars_size; i++)
+    {
+        flag=true;
+        for(int j=0; j<query_size; j++)
+        {
+            if(chars[i+j]!=query[j])
+            {
+                flag=false;
+                break;
+            } 
+        }
+        if(flag)return i;
+    }
+    return -1;
+}
+
+int 
+extractJsonInt(char* raw, const char* query, size_t raw_size, size_t query_size)
+{
+    int ind = searchChars(raw, query, raw_size, query_size, 0) + query_size + 2;
+    return atoi(&raw[ind]);
+}
+
+std::string
+extractJsonStr(char* raw, const char* query, size_t raw_size, size_t query_size){
+    int ind = searchChars(raw, query, raw_size, query_size, 0) + query_size + 3;
+    int len = searchChars(raw, "\"", raw_size, 1, ind) - ind;
+    return std::string(raw, ind, len);
+}
+
+int
+updateweather(weather_data* data)
+{
+    char resp[2048];
+    memset(resp, '\0', sizeof(resp));
+    wifi.http_get_request(addr, payload, header, resp, 2048);
+    data->humidity             = extractJsonInt(resp, "humidity",             sizeof(resp), 8);
+    data->precipitation_chance = extractJsonInt(resp, "daily_chance_of_rain", sizeof(resp), 20);
+    data->temperature          = extractJsonInt(resp, "temp_f",               sizeof(resp), 6);
+    data->wind_speed           = extractJsonInt(resp, "wind_mph",             sizeof(resp), 8);
+    data->weather              = extractJsonStr(resp, "text",                 sizeof(resp), 4).c_str();
+    
+    return 1;
+}
+
+} //namespace
+
+// ====================== Global Definitions =========================
 int
 main()
 {
   //wifi
     printf("Starting demo...\n");
-    wifi.init();
-    printf("Disconnecting from all AP's...\n");
-    wifi.disconnect();
-    printf("Listing all AP's...\n");
-    char ap_list[256];
-    wifi.scan(ap_list, 256);
-    printf("\n%s\n", ap_list);
-    printf("connecting...\n");
-    wifi.connect(ssid,pwd);
+    startWifi();
     printf("Connected! Beginning HTTP get...\n");
-    char resp[1024];
-    const char* addr = "api.weatherapi.com";
-    const char* payload = "/v1/current.json?key=a9e3fb6a760c49699d625304232504&q=Atlanta&aqi=no";
-    const char* header = "Accept: application/json";
-    wifi.http_get_request(addr, payload, header, resp, 1024);
-    printf("%s\n", resp);
-    printf("Finished.");
+    weather_data* data = (weather_data*)malloc(sizeof(weather_data));
+    updateweather(data);
+    printf("Humidity: %d%\n",             data->humidity);
+    printf("Precipitation Chance: %d%\n", data->precipitation_chance);
+    printf("Temperature: %d degrees F\n", data->temperature);
+    printf("Wind Speed: %d mph\n",        data->wind_speed);
+    printf("Weather Description: %s\n",   data->weather);
+    printf("Finished.\n");
     
     
   debug("\r\n[main] Starting up.");
@@ -104,13 +163,6 @@ main()
   debug(" done.");
 
   debug("\r\n[main] Running weather demo...");
-  weather_data* data         = (weather_data*)malloc(sizeof(weather_data));
-  data->humidity             = 35;
-  data->precipitation_chance = 76;
-  data->temperature          = 78;
-  data->wind_speed           = 15;
-  const char* w              = "it is partly cloudy";
-  data->weather              = w;
   while (true) {
     Display_Weather(data);
     ThisThread::sleep_for(1s);
